1f3f24ee4112440b96d626a392875e3fb4a2d05c
Donna Han <89470080+handonna@users.noreply.github.com>
2023-01-27 00:46:48 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,39 +8,39 @@ const minimist = require ("minimist");
 const args = minimist (process.argv.slice());
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-const port = args ["port"] || 3000;
+const port = args["port"] || 3000;
+const host = 'localhost';
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
-fs.readFile("./public/index.html", (err, data) => {
+
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
-if (err) {
-	console.error(err);
-	return;
-}
+fs.readFile('./public/index.html', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
 // Define a const `server` as an arrow function using http.createServer. 
 // Use the documentation for the node.js http module. 
 // The function should have three responses: 
 // 1. status code 200, 
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./public/index.html.
-const server = http.createServer((_, res) => {
-	res.statusCode = 200;
-	res.setHeader = ("Content-Type", "text/html");
-	res.end(data);
-	});
-
+http.createServer(function (req, res)  {
+   
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    
+    res.end(data);
+    
+    console.log(`Server listening on port ${port}`);
+  }).listen(port);  });
 
 
 
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
-server.listen(port, () =>  {
-	console.log(`Server listening on port ${port}`);
-	});
-})
 
 
 // That's it! You're all done!
